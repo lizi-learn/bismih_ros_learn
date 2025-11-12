8a1c7758defcc4290d6b5b421a07c7c44da94f3c
lovez <bismih@qq.com>
2025-11-12 13:57:22 +0800

    clean cache and ignore shell scripts

--- git-push.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add ros_neotic_service_demo README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:lizi-learn/bismih_ros_learn.git
-git push -u origin main
