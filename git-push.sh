238536673706faef1f2123d856015252ce4af464
lovez <bismih@qq.com>
2025-11-12 13:55:15 +0800

    resolve conflict

---
+++ git-push.sh
@@ -0,0 +1,6 @@
+git init
+git add ros_neotic_service_demo README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:lizi-learn/bismih_ros_learn.git
+git push -u origin main
