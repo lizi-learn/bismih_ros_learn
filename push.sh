7a6f835da1725eb0146161e8b904f88ce75e951f
lovez <bismih@qq.com>
2025-11-12 19:13:01 +0800

    Add ros_neotic_web_websocket_demo

---
+++ push.sh
@@ -0,0 +1,29 @@
+git add push.sh README.md
+# git add -f ros_neotic_service_demo
+
+
+# rm -rf .git/modules/ros_neotic_service_demo
+# git rm --cached ros_neotic_service_demo
+
+
+
+# # 1️⃣ 重新添加目录里的文件
+# git add ros_neotic_service_demo/*
+
+# # 2️⃣ 添加其他想提交的文件（可选）
+# git add README.md push.sh tmp/
+
+# # 3️⃣ 提交
+# git commit -m "Restore ros_neotic_service_demo and all project files"
+
+# # 4️⃣ 强制推送覆盖远程
+# git push origin main --force
+
+
+
+git commit -m "Add ros_neotic_web_websocket_demo"
+
+
+git push origin main --force
+
+echo "Pushed to main branch successfully."
